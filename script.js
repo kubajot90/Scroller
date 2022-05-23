7f16cff287a3fc0ed1e32cd24832faa66533b98d
Jakub Jakubowski <kubajot6@wp.pl>
2022-05-23 13:45:21 +0100

    Add scroll feture

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const rootElement = document.querySelector("#root");
   const sections = document.querySelectorAll("section");
 
-  document.addEventListener("mousewhell", function (event) {
+  let currentSectionIndex = 0;
+  let isThrottled = false;
+
+  document.addEventListener("wheel", (event) => {
+    if (isThrottled) return;
+    isThrottled = true;
+
+    setTimeout(() => (isThrottled = false), 1000);
+    const direction = event.wheelDelta < 0 ? 1 : -1;
     console.log(event.wheelDelta);
+
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === sections.length - 1;
+      if (isLastSection) return;
+    } else if (direction === -1) {
+      const isFirstSection = currentSectionIndex === 0;
+      if (isFirstSection) return;
+    }
+    currentSectionIndex += direction;
+    console.log(currentSectionIndex);
+
+    sections[currentSectionIndex].scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
   });
 });
